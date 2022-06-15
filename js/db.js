5b2df8941e5aaf2d5c2b6315eba89523fbdd5abe
Alan Quinones <Alan_QG@outlook.com>
2022-06-14 19:12:22 -0600

    1er

---
+++ js/db.js
@@ -0,0 +1,24 @@
+const casas = [
+    {
+        precio: 2000000,
+        m2: 3195.074,
+        numRoom: 3,
+        numBath: 2,
+        servicios: ["agua","gas"],
+        plantas: 2,
+        ubicacion: "Sendero Las Torres",
+        
+    },
+    {
+        precio: 2300000,
+        m2: 3199.986,
+        numRoom: 4,
+        numBath: 2,
+        servicios: ["agua","gas", "electricidad"],
+        plantas: 2,
+        ubicacion: "Sendero Las Torres",
+    },
+
+]
+
+console.log(casas);
